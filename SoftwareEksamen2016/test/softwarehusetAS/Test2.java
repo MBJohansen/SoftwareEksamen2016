51c99674602380655ef7bf0762e51f34d7de7e8a
Chugkram <Atrofex@gmail.com>
2016-05-08 11:35:11 +0200

    Tests done

--- SoftwareEksamen2016/test/softwarehusetAS/Test2.java
+++ SoftwareEksamen2016/test/softwarehusetAS/Test2.java
@@ -1,15 +1,13 @@
 package softwarehusetAS;
 
-//
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-//
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
-//
 import org.junit.After;
 import org.junit.Test;
 
